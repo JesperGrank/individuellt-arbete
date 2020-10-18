71523f1ed254d19e674e62db2acaba809db21f7c
Jesper Grankvist <jesper.grankvist@hotmail.com>
2020-10-18 22:26:02 +0200

    Finns checkboxar i varje svarsruta.

--- js/quiz.js
+++ js/quiz.js
@@ -4,7 +4,7 @@ class Quiz {
         this.answers = answers;
         this.correct_answers = correct_answers;
         this.question = question;
-        this.given_answers = [];
+        
     }
 
     displayQuestion() {
@@ -12,28 +12,34 @@ class Quiz {
         let tabortdiv = document.getElementsByClassName("answers");
         console.log(tabortdiv);
 
-        if(tabortdiv.length > 0){
-            for(const element of tabortdiv){
+        if (tabortdiv.length > 0) {
+            for (const element of tabortdiv) {
                 document.getElementById("container_answers").removeChild(element)
             }
         }
-       
+
 
 
         Object.values(this.answers).forEach(element => { //object.values gör om ett object till en array 
-            if(element === null) return; //Om svaren = null visas de ej-
+            if (element === null) return; //Om svaren = null visas de ej-
             let divSvar = document.createElement("div");
-            divSvar.className="answers";
+            divSvar.className = "answers";
             document.getElementById("container_answers").appendChild(divSvar);
             divSvar.innerHTML = element;
-
-            divSvar.addEventListener("click", function(e){
-                console.log(divSvar)
-            })
+            /* <div class="answers"><input type="checkbox"> if (i != 5)</div> */
+            let divCheckbox = document.createElement("input"); //Skapar en input
+            divCheckbox.setAttribute("type", "checkbox"); //Sätter type till checkbox
+            divSvar.appendChild(divCheckbox); //Skickar in checkboxen i varje möjligt svar.
+           
         });
 
     }
 
+    checkResults(){
+        let results = [];
+        results.push(this.correct_answers)
+        console.log(results)
+    }
     /*fetch(){
         let generera = document.getElementById("generera");
         generera.addEventListener("click", function (e) {
